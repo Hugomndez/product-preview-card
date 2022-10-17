21b76ef3f33a8ae704f2b3c6b3cf04f5164b37e5
Hugomndez <hello@hugomendez.dev>
2022-10-17 13:11:15 -0500

    style: productCard

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -1,15 +1,35 @@
 import { Button } from 'components';
+import styles from './ProductCard.module.css';
+
 const ProductCard = () => {
   return (
-    <div>
-      <h1>Perfume</h1>
-      <h2>Preview Gabrielle Essence Eau De Parfum</h2>
-      <p>
-        A floral, solar and voluptuous interpretation composed by Olivier Polge,
-        Perfumer-Creator for the House of CHANEL.
-      </p>
-      <span>$149.99</span> <span>$169.99</span>
-      <Button />
+    <div className={styles.card}>
+      <picture>
+        <source
+          srcSet='/image-product-desktop.jpg'
+          media='(min-width: 640px)'
+        />
+        <img
+          className={styles.image}
+          src='/image-product-mobile.jpg'
+          alt='Perfume - Gabrielle Essence'
+          decoding='async'
+          loading='eager'
+        />
+      </picture>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Perfume</h1>
+        <h2 className={styles.subTitle}>Gabrielle Essence Eau De Parfum</h2>
+        <p className={styles.description}>
+          A floral, solar and voluptuous interpretation composed by Olivier
+          Polge, Perfumer-Creator for the House of CHANEL.
+        </p>
+        <div className={styles.pricingWrapper}>
+          <span className={styles.discount}>$149.99</span>
+          <span className={styles.price}>$169.99</span>
+        </div>
+        <Button />
+      </div>
     </div>
   );
 };
